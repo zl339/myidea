1db1426daf0ebefe8a71de8022938ac294de46f2
yourname <your@email.com>
2019-04-11 14:22:05 +0800

    小改下

--- Mygit339/src/com/itheima/git/mydemo.java
+++ Mygit339/src/com/itheima/git/mydemo.java
@@ -4,6 +4,7 @@ public class mydemo {
     public static void main(String[] args) {
         System.out.println("使用git");
         System.out.println("我克隆完了");
+        System.out.println("更改下");
     }
 
 }
